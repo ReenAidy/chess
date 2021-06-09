6bdc1aaf8309a8ab12f916f2d60d9b8046fb8320
Asus <gawaliamol.ar@gmail.com>
2021-06-09 11:15:18 +0530

    chess project

---
+++ Chess/Chess_main.py
@@ -0,0 +1,75 @@
+import pygame
+from Chess import Chess_engine
+
+pygame.init()
+WIDTH = HEIGHT = 520
+DIMENSION = 8
+SQ_SIZE = WIDTH // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+
+def loadImages():
+    pieces = ["wp", "wR", "wN", "wB", "wQ", "wK", "bp", "bR", "bN", "bB", "bQ", "bK"]
+    for piece in pieces:
+        IMAGES[piece] = pygame.transform.scale(pygame.image.load("image/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+def main():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    screen.fill(pygame.Color("white"))
+    gs = Chess_engine.GameState()
+    loadImages()
+    running = True
+    sqSelected = ()
+    playerClicks = []
+    while running:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                running = False
+            elif e.type == pygame.MOUSEBUTTONDOWN:
+                location = pygame.mouse.get_pos()
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+                if sqSelected == (row, col):
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:
+                    move = Chess_engine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected=()
+                    playerClicks=[]
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        pygame.display.flip()
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+
+def drawBoard(screen):
+    colors = [pygame.Color("white"), pygame.Color("grey")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r + c) % 2)]
+            pygame.draw.rect(screen, color, pygame.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], pygame.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+if __name__ == '__main__':
+    main()
